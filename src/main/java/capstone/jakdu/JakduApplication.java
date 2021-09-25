aeb872594b5aabc7a8ebd4274858ac30d33bfed6
Youhoseong <h0song@naver.com>
2021-09-26 00:07:37 +0900

    docs: Reademe setting

---
+++ src/main/java/capstone/jakdu/JakduApplication.java
@@ -0,0 +1,13 @@
+package capstone.jakdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakduApplication.class, args);
+	}
+
+}
