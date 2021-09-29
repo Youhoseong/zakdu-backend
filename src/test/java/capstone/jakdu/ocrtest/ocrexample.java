1e760dab4017d48abbf5c3ee585829a01f2f50c3
Youhoseong <h0song@naver.com>
2021-09-29 23:08:42 +0900

    test: get textposition of pdf text

--- src/test/java/capstone/jakdu/ocrtest/ocrexample.java
+++ src/test/java/capstone/jakdu/ocrtest/ocrexample.java
@@ -1,24 +1,62 @@
 package capstone.jakdu.ocrtest;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.TextPosition;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 
 public class ocrexample {
 
+    PDFTextStripper reader = new PDFTextStripper() {
+        boolean startOfLine = true;
+
+        @Override
+        protected void startPage(PDPage page) throws IOException {
+            startOfLine = true;
+            super.startPage(page);
+        }
+
+        @Override
+        protected void writeLineSeparator() throws IOException {
+            startOfLine = true;
+            super.writeLineSeparator();
+        }
+
+        @Override
+        protected void writeString(String text, List<TextPosition> textPositions) throws IOException {
+
+            if (startOfLine)
+            {
+                System.out.println("text = " + text);
+                TextPosition firstPosition = textPositions.get(0);
+                writeString(String.format("[%s %s]",firstPosition.getXDirAdj(), firstPosition.getYDirAdj()));
+                startOfLine = false;
+            }
+            super.writeString(text, textPositions);
+        }
+
+    };
+
+    public ocrexample() throws IOException {
+    }
+
     @Test
     public void getAllPageExtractPDF() throws IOException {
-        String fileName = "수력충전본문목차.pdf";
-        File source = new File(fileName);
+        String fileName = "수력충전본문목차.pdf";
+        String filePath = System.getProperty("user.dir") + "/"+ fileName;
+        File source = new File(filePath);
         PDDocument pdfDoc = PDDocument.load(source);
-        PDFTextStripper reader = new PDFTextStripper();
-        reader.setSortByPosition(true);
+        //reader.setSortByPosition(true);
+
         String text = reader.getText(pdfDoc);
         System.out.println(text);
+
     }
     
     @Test
@@ -34,8 +72,6 @@ public class ocrexample {
         reader.setEndPage(i+2);
         reader.setSortByPosition(true);
         String pageText = reader.getText(pdfDoc);
-
-
         System.out.println(pageText);
     }
 
