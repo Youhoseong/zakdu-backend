0b53ebb6dc49f47b4659e77475699099921cc3a1
heoboseong7 <boseong844@gmail.com>
2021-11-29 19:11:09 +0900

    refactor: Download pdf, fix: unmatched start page

--- src/main/java/capstone/jakdu/Book/controller/BookDownloadController.java
+++ src/main/java/capstone/jakdu/Book/controller/BookDownloadController.java
@@ -1,11 +1,13 @@
 package capstone.jakdu.Book.controller;
 
-import capstone.jakdu.Book.object.dto.BookDownloadDto;
+import capstone.jakdu.Book.object.dto.BookFileNameDto;
 import capstone.jakdu.Book.service.BookDownloadService;
 import capstone.jakdu.Book.service.EPUBBookEncryptService;
 import capstone.jakdu.Common.response.ResponseDto;
 import capstone.jakdu.Common.response.StatusEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @RestController
@@ -24,9 +27,9 @@ public class BookDownloadController {
     private final EPUBBookEncryptService epubBookEncryptService;
 
     @GetMapping("/pdf")
-    public ResponseDto downloadPdfBook(@RequestParam("id") Long id) throws IOException {
+    public ResponseDto getFileNames(@RequestParam("id") Long id) throws IOException {
         // 유저 구매여부 확인, 유저 정보도 추가로 받아야 함
-        BookDownloadDto bookDownloadDto = bookDownloadService.downloadBook(id);
+        BookFileNameDto bookDownloadDto = bookDownloadService.downloadBook(id);
 
         if(bookDownloadDto == null) {
             return new ResponseDto(StatusEnum.INTERNAL_SERVER_ERROR, "internal server error", null);
@@ -36,8 +39,13 @@ public class BookDownloadController {
         }
     }
 
+    @GetMapping("/pdf-2")
+    public ResponseEntity<InputStreamResource> downloadPdfBook2(@RequestParam("id") Long id) throws FileNotFoundException {
+        return bookDownloadService.downloadPdfBook(id);
+    }
+
     @GetMapping("/pdf_lock")
-    public ResponseDto downloadPdfBook() throws IOException {
+    public ResponseDto downloadLockPdf() throws IOException {
         byte[] lockPdf = bookDownloadService.downloadLockPdf();
         return new ResponseDto(StatusEnum.OK, "success", lockPdf);
     }
--- src/main/java/capstone/jakdu/Book/service/BookDownloadService.java
+++ src/main/java/capstone/jakdu/Book/service/BookDownloadService.java
@@ -2,13 +2,19 @@ package capstone.jakdu.Book.service;
 
 import capstone.jakdu.Book.domain.FileStream;
 import capstone.jakdu.Book.domain.PDFBook;
-import capstone.jakdu.Book.object.dto.BookDownloadDto;
+import capstone.jakdu.Book.object.dto.BookFileNameDto;
 import capstone.jakdu.Book.repository.PDFBookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,20 +27,39 @@ public class BookDownloadService {
     private final PDFBookRepository pdfBookRepository;
     private final PDFBookEncryptService pdfBookEncryptService;
 
-    public BookDownloadDto downloadBook(Long bookId) throws IOException {
+    public BookFileNameDto downloadBook(Long bookId) throws IOException {
+        PDFBook pdfBook = pdfBookRepository.findById(bookId).get();
+
+        FileStream bookFile = pdfBook.getBookFile();
+
+        String fileName = bookFile.getFileName();
+        String coverFileName = pdfBook.getBookCover().getFileName();
+
+        return new BookFileNameDto(bookId, fileName, coverFileName);
+    }
+
+    public ResponseEntity<InputStreamResource> downloadPdfBook(Long bookId) throws FileNotFoundException {
         PDFBook pdfBook = pdfBookRepository.findById(bookId).get();
 
         FileStream bookFile = pdfBook.getBookFile();
 
         String filePath = pdfBookEncryptService.getEncPdfPath();
         String fileName = bookFile.getFileName();
-        String coverFileName = pdfBook.getBookCover().getFileName();
+        File file = new File(filePath + fileName);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
 
-        File pdfBookFile = new File(filePath + fileName);
-        Path path = Paths.get(pdfBookFile.getAbsolutePath());
-        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
 
-        return new BookDownloadDto(bookId, fileName, coverFileName, resource.getByteArray());
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+
     }
 
     public byte[] downloadLockPdf() throws IOException {
